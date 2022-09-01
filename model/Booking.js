74e24c244a5deaa8a0b9ca559c8cedd0825b4b25
Dio Fitriadi <fitriadidio@gmail.com>
2022-09-01 14:34:19 +0700

    feat: "fix get booking"

--- model/Booking.js
+++ model/Booking.js
@@ -4,9 +4,10 @@ const db = require('../helper/db_connection')
 module.exports = {
     get: (req, res)=> {
       return new Promise((resolve, reject)=> {
-        const sql = `SELECT * FROM booking LEFT JOIN movies on booking.id_movies = movies.id_movies LEFT JOIN booking.id_cinema = cinema.id_cinema ORDER BY booking.created_at DESC` 
+        const sql = `SELECT * FROM booking LEFT JOIN movies on booking.id_movies = movies.id_movies LEFT JOIN cinema ON booking.id_cinema = cinema.id_cinema ORDER BY booking.created_at DESC` 
         db.query(sql,(err, results)=> {
           if(err) {
+            console.log(err)
             reject({message: "ada error"})
           }
           resolve({
@@ -39,17 +40,17 @@ module.exports = {
     },
     update: (req, res) => {
       return new Promise((resolve, reject)=> {
-        const {id} = req.params
-        db.query(`SELECT * FROM booking where id=${id}`,(err, results)=> {
+        const {id_booking} = req.params
+        db.query(`SELECT * FROM booking WHERE id_booking=${id_booking}`,(err, results)=> {
           if(err) {res.send({message: "ada error"})}
       
           const previousData = {
             ...results[0],
             ...req.body
           }
-          const {title, date_time, cinema, number_of_ticket, total_payment, payment_method} = previousData
+          const {id_movies , id_cinema, date_playing, time, seat} = previousData
       
-          db.query(`UPDATE booking SET title='${title}', date_time='${date_time}', cinema='${cinema}', number_of_ticket='${number_of_ticket}', total_payment='${total_payment}', payment_method='${payment_method}' WHERE id=${id}`,(err, results)=> {
+          db.query(`UPDATE booking SET id_movies='${id_movies}', id_cinema='${id_cinema}', date_playing='${date_playing}', time='${time}', seat='${seat}' WHERE id_booking=${id_booking}`,(err, results)=> {
             if(err) {
               console.log(err)
               reject({message: "ada error"})
@@ -60,14 +61,13 @@ module.exports = {
               data: results
             })
           })
-      
         })
       })
     },
     remove:(req, res)=> {
       return new Promise((resolve, reject)=> {
-        const {id} = req.params
-        db.query(`DELETE FROM booking where id=${id}`,(err, results)=> {
+        const {id_booking} = req.params
+        db.query(`DELETE FROM booking where id=${id_booking}`,(err, results)=> {
           if(err) {reject({message: "ada error"})}
           resolve({
             message: "delete booking success",
